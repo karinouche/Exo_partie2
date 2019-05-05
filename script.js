c9744987f47579042dc3217d7f957bab11ccb5da
karinouche <50261642+karinouche@users.noreply.github.com>
2019-05-05 20:13:08 +0200

    Ajout et modification de ligne de code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert ("Hello")
+alert ("Bonjour, ci-dessous des exemples de code en javascript")
+
+var age=25; // Tout ce qui suit est un commentaire
+
+// les fonctions
+function somme(a,b) {
+      var sum=a+b;
+      return sum;
+   }
